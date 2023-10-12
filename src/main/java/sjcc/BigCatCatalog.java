3e7a748cd07f37ffe299ae814ad9513412b275a3
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2023-10-12 15:25:59 -0700

    Added additional methods into the BigCatCatalog (list all cats and finding cats)

--- src/main/java/sjcc/BigCatCatalog.java
+++ src/main/java/sjcc/BigCatCatalog.java
@@ -25,17 +25,37 @@ public class BigCatCatalog {
         bigCats.add(newCat);
         System.out.println("A new cat has been created");
     }
-    
+
     //delete cat by name
     public void deleteCat(String name) {
         for (Pan cat : bigCats) {
-            if (cat.name().equalsIfnoreCase(name))
+            if (cat.name().equalsIgnoreCase(name)){
                 bigCats.remove(cat);
-            System.out.println(name + " has been successfully deleted from the catatog");
-            return;
+                System.out.println(name + " has been successfully deleted from the catatog");
+                return;
+            }
+
+        }
+    
+        System.out.println(name + " is not found in the catalog.");
+    }
+
+    //finding cats that match part of the search term
+    public List<Pan> findCats(String searchTerm) {
+        List<Pan> foundCats = new ArrayList<>();
+        for (Pan cat : bigCats) {
+            if (cat.name().toLowerCase().contains(searchTerm.toLowerCase())) {
+                foundCats.add(cat);
+            }
         }
 
+        return foundCats;
     }
-    
-    System.out.println(name + " is not found in the catalog.");
+
+    public void listAllCats() {
+        for (Pan cat : bigCats) {
+            System.out.println(cat.toString());
+        }
+    }
 }
+

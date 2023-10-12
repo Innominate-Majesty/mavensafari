7a1d3da29e06b3550a6245709d49dfef8ea6f0f3
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2023-10-12 13:23:06 -0700

    create Display menu
    created the display menu

--- src/main/java/sjcc/App.java
+++ src/main/java/sjcc/App.java
@@ -1,13 +1,26 @@
 package sjcc;
 
 /**
- * Hello world!
- *
+ * Venus H
+ * Class: CIS 59 - Object Oriented Programming
+ * Date: 10-12-2023
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        public static void printDisplay(){
+            System.out.println("  ");
+            System.out.println("****************************************\n");
+            System.out.println("    Big Cat Catelog by Venus   \n");;
+            System.out.println("****************************************\n");
+            System.out.println("c    :    [C]reate a big cat]");
+            System.out.println("d    :    [D]elete a big cat]");
+            System.out.println("f    :    [F]ind a big cat");
+            System.out.println("l    :    [L]ist all big cats");
+            System.out.println("q    :    [Q]uit");
+            System.out.println("****************************************\n");
+    
+        }
     }
 }

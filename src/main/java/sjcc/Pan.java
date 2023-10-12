fd8673243d316502d87dd4a68e9de2efe26c9923
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2023-10-12 15:29:54 -0700

    added methods and attributes to Pan.java
    definind the name() and constructor for pantheras

--- src/main/java/sjcc/Pan.java
+++ src/main/java/sjcc/Pan.java
@@ -1,5 +1,20 @@
 package sjcc;
 
-public class Pan {
+public class Pan extends PanGPS {
+    
+    private String name;
+    private String species;
+
+    //constructor
+    public Pan(String name, String species) {
+        super(name);
+        this.name = name;
+        this.species = species;
+    }
+
+    //name() method
+    public String name() {
+        return this.name;
+    }
     
 }
